e835af957c4075a7c97a4ae095cdcafd4e5696dd
Bryan Spahr <bspahr314@gmail.com>
2014-01-27 17:10:38 -0600

    initial work

---
+++ Assets/CameraTrackMouse.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTrackMouse : MonoBehaviour {
+	
+	public float amount = 50f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis("Mouse Y")*amount*Time.deltaTime;
+		float v = Input.GetAxis("Mouse X")*amount*Time.deltaTime;
+		
+		transform.Rotate(h,v,0);
+	}
+}
